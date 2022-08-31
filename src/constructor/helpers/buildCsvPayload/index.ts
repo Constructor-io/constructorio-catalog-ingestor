6a8004654368de1c3e54f0183f317252bac5ddd6
Pedro Bini <pedro.bini@constructor.io>
2022-08-31 10:48:31 -0300

    docs: add jsdoc

--- src/constructor/helpers/buildCsvPayload/index.ts
+++ src/constructor/helpers/buildCsvPayload/index.ts
@@ -1,6 +1,11 @@
 import { CatalogIngestionPayload } from "catalogIngestor/types";
 import { CsvPayload } from "constructor/api/catalog/ingestCatalogCsv";
 
+/**
+ * Builds the csv payload for the catalog ingestion.
+ * @param data The data that was inputted into the catalog ingestor.
+ * @returns The csv payload.
+ */
 export function buildCsvPayload(_data: CatalogIngestionPayload): CsvPayload {
   // TODO: Implement buildCsvPayload.
 
