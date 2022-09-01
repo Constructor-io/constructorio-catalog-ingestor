7464e2bf1fd1b6c7569fb8280b882df1628506d3
Pedro Bini <pedro.bini@constructor.io>
2022-09-01 17:19:28 -0300

    test: cover fetching data in catalog ingestor

--- src/catalogIngestor/index.spec.ts
+++ src/catalogIngestor/index.spec.ts
@@ -1,23 +1,60 @@
+import { catalogIngestionPayloadFactory } from "../../test/factories/catalogIngestionPayload.factory";
+
+import { CatalogIngestionPayload } from "./types";
+
+import * as ingestCatalogCsv from "constructor/api/catalog/ingestCatalogCsv";
+import * as buildCsvPayload from "constructor/helpers/buildCsvPayload";
 import { CatalogIngestor } from "catalogIngestor";
 
 describe("CatalogIngestor", () => {
-  it("should allow initializing with a new api token", () => {
-    const catalogIngestor = new CatalogIngestor({
+  let getData: () => Promise<CatalogIngestionPayload>;
+  let catalogIngestor: CatalogIngestor;
+
+  beforeEach(() => {
+    catalogIngestor = new CatalogIngestor({
       constructorApiToken: "api-token",
     });
 
+    getData = jest
+      .fn()
+      .mockResolvedValue(catalogIngestionPayloadFactory.build());
+
+    jest.spyOn(buildCsvPayload, "buildCsvPayload").mockResolvedValue({
+      groups: "groups",
+      items: "items",
+      variations: "variations",
+    });
+
+    jest.spyOn(ingestCatalogCsv, "ingestCatalogCsv").mockResolvedValue();
+  });
+
+  it("should allow initializing with a new api token", () => {
     expect(catalogIngestor.credentials.constructorApiToken).toBe("api-token");
   });
 
-  /**
-   * TODO: Add tests for ingesting catalogs.
-   */
   describe("ingest", () => {
     describe("when executing the promise to obtain the data", () => {
-      it.todo("should call the promise with correct params");
+      it("should call the promise with correct params", async () => {
+        await catalogIngestor.ingest(getData);
+
+        expect(getData).toHaveBeenCalledTimes(1);
+      });
 
       describe("when the promise fails", () => {
-        it.todo("should not proceed with the ingestion");
+        beforeEach(() => {
+          getData = jest.fn().mockRejectedValue(new Error("foo!"));
+        });
+
+        it("should throw the error", async () => {
+          await expect(catalogIngestor.ingest(getData)).rejects.toThrow("foo!");
+        });
+
+        it("should not proceed with the ingestion", async () => {
+          await expect(catalogIngestor.ingest(getData)).rejects.toThrow("foo!");
+
+          expect(buildCsvPayload.buildCsvPayload).not.toHaveBeenCalled();
+          expect(ingestCatalogCsv.ingestCatalogCsv).not.toHaveBeenCalled();
+        });
       });
     });
 
