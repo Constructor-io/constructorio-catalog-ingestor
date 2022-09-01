48d2d512e6963595b82d0bea5971a2d2c2cdedef
Pedro Bini <pedro.bini@constructor.io>
2022-09-01 16:52:56 -0300

    chore: handle expanding nested arrays in csv payload

--- src/constructor/helpers/buildCsvPayload/index.ts
+++ src/constructor/helpers/buildCsvPayload/index.ts
@@ -1,6 +1,7 @@
 import Papa from "papaparse";
+import * as R from "remeda";
 
-import { CatalogIngestionPayload } from "catalogIngestor/types";
+import { CatalogIngestionPayload, KeyValue } from "catalogIngestor/types";
 import { CsvPayload } from "constructor/api/catalog/ingestCatalogCsv";
 
 /**
@@ -26,21 +27,146 @@ export async function buildCsvPayload(
 
 /**
  * Parses an array into csv.
- * @param type The type of data being parsed.
- * @param data The data to be parsed into csv.
+ * @param type The type of the array.
+ * @param objects The objects to be parsed into csv.
  * @returns The csv.
  */
 async function toCsv<T>(
   type: keyof CatalogIngestionPayload,
-  data: T[]
+  objects: T[]
 ): Promise<string | undefined> {
-  if (!data.length) return await Promise.resolve(undefined);
+  if (!objects.length) return await Promise.resolve(undefined);
 
-  const csv = Papa.unparse(data, {
-    columns: columnOrders[type],
+  const proxyObjects = objects.map((object) => toCsvProxyObject(object));
+  const columns = getColumnsFromProxyObjects(type, proxyObjects);
+
+  const csv = Papa.unparse(proxyObjects, {
+    newline: "\n",
+    header: true,
+    columns,
   });
 
   return await Promise.resolve(csv);
+}
+
+/**
+ * Returns a proxy object to be transformed to csv.
+ * @param object The object to be transformed to csv.
+ * @returns The proxy object.
+ */
+function toCsvProxyObject<T, TOut>(object: T): TOut {
+  const keys = R.keys(object as Record<string, unknown>);
+
+  const result = R.reduce(
+    keys,
+    (acc, key) => {
+      const value = object[key as keyof T];
+
+      if (isNestedField(key)) {
+        const pairs = value as unknown as KeyValue[];
+        return pairs.length ? expandNestedArray(acc, key, pairs) : acc;
+      }
+
+      return {
+        ...acc,
+        [key]: value,
+      };
+    },
+    {}
+  );
+
+  return result as TOut;
+}
+
+/**
+ * Expands a nested array into the current proxy object.
+ * @param acc The current proxy object.
+ * @param key The key of the nested array.
+ * @param pairs The key,value pairs of the nested array.
+ * @returns The expanded proxy object.
+ */
+function expandNestedArray<TOut>(
+  acc: TOut,
+  key: string,
+  pairs: KeyValue[]
+): TOut {
+  return pairs.reduce((acc, pair) => {
+    return {
+      ...acc,
+      [getNestedFieldTransformedName(key, pair.key)]: pair.value,
+    };
+  }, acc);
+}
+
+/**
+ * Checks if a field is a nested field.
+ * @param name The name of the field.
+ * @returns Whether the field is a nested field.
+ */
+function isNestedField(name: string): boolean {
+  return ["metadata", "facets"].includes(name);
+}
+
+/**
+ * Gets the transformed name of a nested field.
+ * @param name The name of the field.
+ * @param key The key of the nested field.
+ * @returns The name of the transformed nested field.
+ */
+function getNestedFieldTransformedName(name: string, key: string): string {
+  const prefix = {
+    metadata: "metadata",
+    facets: "facet",
+  }[name];
+
+  return `${prefix}:${key}`;
+}
+
+/**
+ * Builds the ordered csv columns from the proxy objects.
+ * @param type The type of the array.
+ * @param proxyObjects The proxy proxyObjects to be parsed into csv.
+ * @returns The csv columns.
+ */
+function getColumnsFromProxyObjects<T>(
+  type: keyof CatalogIngestionPayload,
+  proxyObjects: T[]
+): string[] {
+  const order = columnOrders[type];
+
+  const uniqueColumns = R.pipe(
+    proxyObjects,
+    R.map((object) => object as Record<string, unknown>),
+    R.map((object) => R.keys(object)),
+    (keys) => R.flatten(keys),
+    (keys) => R.uniq(keys)
+  );
+
+  const facetColumns = R.pipe(
+    uniqueColumns,
+    R.filter((column) => column.startsWith("facet:"))
+  );
+
+  const metadataColumns = R.pipe(
+    uniqueColumns,
+    R.filter((column) => column.startsWith("metadata:"))
+  );
+
+  const commonColumns = R.pipe(
+    uniqueColumns,
+    R.filter(
+      (column) => ![...facetColumns, ...metadataColumns].includes(column)
+    )
+  );
+
+  const sortedCommonColumns = R.pipe(
+    commonColumns,
+    R.sort((a, b) => {
+      return order.indexOf(a) - order.indexOf(b);
+    })
+  );
+
+  return [...sortedCommonColumns, ...facetColumns, ...metadataColumns];
 }
 
 const columnOrders: Record<keyof CatalogIngestionPayload, string[]> = {
@@ -54,16 +180,6 @@ const columnOrders: Record<keyof CatalogIngestionPayload, string[]> = {
     "keywords",
     "group_ids",
     "active",
-    "facets",
-    "metadata",
   ],
-  variations: [
-    "variation_id",
-    "item_id",
-    "item_name",
-    "image_url",
-    "active",
-    "facets",
-    "metadata",
-  ],
+  variations: ["variation_id", "item_id", "item_name", "image_url", "active"],
 };
