211e97c4465d9b4191c5f2f12652b037bc83d1b8
Pedro Bini <pedro.bini@constructor.io>
2022-09-01 19:30:43 -0300

    chore: remove config.getHeaders

--- src/constructor/api/config.spec.ts
+++ src/constructor/api/config.spec.ts
@@ -4,12 +4,4 @@ describe("config", () => {
   it("should have a baseUrl", () => {
     expect(config.baseUrl).toBe("https://ac.cnstrc.com");
   });
-
-  describe(config.getHeaders, () => {
-    it("should return an object with an Authorization header", () => {
-      expect(config.getHeaders("foo")).toEqual({
-        Authorization: "Basic Zm9v",
-      });
-    });
-  });
 });
--- src/constructor/api/config.ts
+++ src/constructor/api/config.ts
@@ -3,10 +3,4 @@
  */
 export const config = {
   baseUrl: "https://ac.cnstrc.com",
-
-  getHeaders: function (apiToken: string) {
-    return {
-      Authorization: `Basic ${Buffer.from(apiToken).toString("base64")}`,
-    };
-  },
 };
