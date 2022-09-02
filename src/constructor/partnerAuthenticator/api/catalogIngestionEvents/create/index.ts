339263acd126da94197b6a2a5f62e8300c3ddc29
Pedro Bini <pedro.bini@constructor.io>
2022-09-02 15:29:05 -0300

    chore: add api module to create new ingestion events

---
+++ src/constructor/partnerAuthenticator/api/catalogIngestionEvents/create/index.ts
@@ -0,0 +1,32 @@
+import got from "got";
+
+import { config } from "../../config";
+
+/**
+ * Creates a new catalog ingestion event via API.
+ * @param payload The payload.
+ */
+export async function createIngestionEvent(
+  connectionId: string,
+  payload: Payload
+): Promise<void> {
+  await got
+    .post({
+      url: `${config.baseUrl}/catalog-ingestion-events/create/${connectionId}`,
+      body: JSON.stringify(payload),
+    })
+    .catch(() => {
+      console.warn(
+        "[Ingestor] Failed to create catalog ingestion event. Are your credentials correct?"
+      );
+    });
+}
+
+interface Payload {
+  success: boolean;
+  cioTaskId: string | null;
+  countOfGroups: number;
+  countOfItems: number;
+  countOfVariations: number;
+  totalIngestionTimeMs: number;
+}
